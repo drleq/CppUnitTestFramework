3910d256f49294bb60aeb529e13101c029faddd4
Andrew Condie <Andrew.Condie@eu.medical.canon>
2019-01-09 18:20:01 +0000

    Created initial ToString() tests. Updated CppUnitTestFramework.hpp with changes from other projects.

--- CppUnitTestFramework.hpp
+++ CppUnitTestFramework.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <array>
+#include <cstring>
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <memory>
 #include <optional>
 #include <sstream>
 #include <string_view>
@@ -398,7 +400,17 @@ namespace CppUnitTestFramework {
             } else if constexpr (std::is_enum_v<T>) {
                 // Enum -> [<name>] <number>
                 std::ostringstream ss;
-                ss << "[" << typeid(T).name() << "] " << static_cast<std::underlying_type_t<T>>(value);
+                if constexpr (sizeof(std::underlying_type_t<T>) == sizeof(char)) {
+                    ss << "[" << typeid(T).name() << "] " << static_cast<int>(value);    
+                } else {
+                    ss << "[" << typeid(T).name() << "] " << static_cast<std::underlying_type_t<T>>(value);
+                }
+                return ss.str();
+
+            } else if constexpr (std::is_floating_point_v<T>) {
+                // float|double -> <number>
+                std::ostringstream ss;
+                ss << value;
                 return ss.str();
 
             } else {
@@ -483,17 +495,54 @@ namespace CppUnitTestFramework {
                 std::ostringstream ss;
                 ss << "Expected exception [" << typeid(TException).name() << "] but caught ["
                     << typeid(e).name() << ": " << e.what() << "]";
-                return AssertException(ss.str().c_str());
+                return AssertException(ss.str());
             } catch (...) {
                 std::ostringstream ss;
                 ss << "Expected exception [" << typeid(TException).name() << "] but caught another";
-                return AssertException(ss.str().c_str());
+                return AssertException(ss.str());
             }
 
             std::ostringstream ss;
             ss << "Expected exception [" << typeid(TException).name() << "] but none was thrown";
-            return AssertException(ss.str().c_str());
+            return AssertException(ss.str());
         }
+
+        //----------------------------------------------------------------------------------------------------
+
+        inline std::optional<AssertException> Close(float left, float right, float percentage_tolerance) {
+            float diff = right - left;
+            if (diff < 0.0f) { diff = -diff; }
+            float percentage_left = diff / left;
+            float percentage_right = diff / right;
+
+            if (percentage_left <= percentage_tolerance && percentage_right <= percentage_tolerance) {
+                return std::nullopt;
+            }
+
+            std::ostringstream ss;
+            ss << "[" + Ext::ToString(left) + "] == [" + Ext::ToString(right) + "]: ";
+            ss << "[" << diff << "] exceeds " << percentage_tolerance << "%";
+            return AssertException(ss.str());
+        }
+
+        //----------------------------------------------------------------------------------------------------
+
+        inline std::optional<AssertException> Close(double left, double right, double percentage_tolerance) {
+            double diff = right - left;
+            if (diff < 0.0f) { diff = -diff; }
+            double percentage_left = diff / left;
+            double percentage_right = diff / right;
+
+            if (percentage_left <= percentage_tolerance && percentage_right <= percentage_tolerance) {
+                return std::nullopt;
+            }
+
+            std::ostringstream ss;
+            ss << "[" + Ext::ToString(left) + "] == [" + Ext::ToString(right) + "]: ";
+            ss << "[" << diff << "] exceeds " << percentage_tolerance << "%";
+            return AssertException(ss.str());
+        }
+
     }
 
     //--------------------------------------------------------------------------------------------------------
@@ -629,6 +678,8 @@ void TestCase_##TestName::Run()
 #define REQUIRE_NULL(Expression)   CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Throw, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::IsNull((Expression), #Expression))
 #define REQUIRE_THROW(ExceptionType, Expression) \
     CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Throw, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::Throws<ExceptionType>([&] { Expression; }))
+#define REQUIRE_CLOSE(Left, Right, Percentage) \
+    CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Throw, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::Close((Left), (Right), (Percentage)))
 
 #define CHECK(Expression)        CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Continue, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::IsTrue((Expression), #Expression))
 #define CHECK_TRUE(Expression)   CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Continue, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::IsTrue((Expression), #Expression))
@@ -637,6 +688,8 @@ void TestCase_##TestName::Run()
 #define CHECK_NULL(Expression)   CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Continue, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::IsNull((Expression), #Expression))
 #define CHECK_THROW(ExceptionType, Expression) \
     CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Continue, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::Throws<ExceptionType>([&] { Expression; }))
+#define CHECK_CLOSE(Left, Right, Percentage) \
+    CppUnitTestFramework::CommonFixture::HandleAssert(CppUnitTestFramework::AssertType::Continue, _CPPUTF_ASSERT_LOCATION, CppUnitTestFramework::Assert::Close((Left), (Right), (Percentage)))
 
 //------------------------------------------------------------------------------------------------------------
 
---
+++ Tests/ToStringTest.cpp
@@ -0,0 +1,100 @@
+#include "CppUnitTestFramework.hpp"
+
+using namespace CppUnitTestFramework;
+
+namespace {
+    struct ToStringTest {
+        enum Untyped { Value1 = 10, Value2 = -20 };
+        enum class Typed : uint8_t {
+            Value1 = 10,
+            Value2 = 200
+        };
+    };
+
+    struct CustomType {
+        int Value;
+    };
+}
+
+namespace CppUnitTestFramework::Ext {
+    std::string ToString(const CustomType& value) {
+        std::ostringstream ss;
+        ss << "[CustomType] " << value.Value;
+        return ss.str();
+    }
+}
+
+namespace CppUnitTestFrameworkTest {
+
+    TEST_CASE(ToStringTest, Nullptr) {
+        CHECK_EQUAL(Ext::ToString(nullptr), "nullptr");
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, Pointer) {
+        const void* zero_ptr = 0;
+        const int* int_ptr = reinterpret_cast<int*>(0x123456);
+        const float* float_ptr = reinterpret_cast<float*>(0x123456789abcdef);
+
+        CHECK_EQUAL(Ext::ToString(zero_ptr), "0x0000000000000000");
+        CHECK_EQUAL(Ext::ToString(int_ptr), "0x0000000000123456");
+        CHECK_EQUAL(Ext::ToString(float_ptr), "0x0123456789abcdef");
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, Enum) {
+        CHECK_EQUAL(Ext::ToString(Untyped::Value1), "[enum `anonymous namespace'::ToStringTest::Untyped] 10");
+        CHECK_EQUAL(Ext::ToString(Untyped::Value2), "[enum `anonymous namespace'::ToStringTest::Untyped] -20");
+        CHECK_EQUAL(Ext::ToString(Typed::Value1), "[enum `anonymous namespace'::ToStringTest::Typed] 10");
+        CHECK_EQUAL(Ext::ToString(Typed::Value2), "[enum `anonymous namespace'::ToStringTest::Typed] 200");
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, FloatingPoint) {
+        SECTION("float") {
+            CHECK_EQUAL(Ext::ToString(1.0f), "1");
+            CHECK_EQUAL(Ext::ToString(1.5f), "1.5");
+            CHECK_EQUAL(Ext::ToString(1e-10f), "1e-10");
+            CHECK_EQUAL(Ext::ToString(-1.234f), "-1.234");
+        }
+
+        SECTION("double") {
+            CHECK_EQUAL(Ext::ToString(1.0), "1");
+            CHECK_EQUAL(Ext::ToString(1.5), "1.5");
+            CHECK_EQUAL(Ext::ToString(1e-10), "1e-10");
+            CHECK_EQUAL(Ext::ToString(-1.234), "-1.234");
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, Integers) {
+        CHECK_EQUAL(Ext::ToString(static_cast<int8_t>(-123)), "-123");
+        CHECK_EQUAL(Ext::ToString(static_cast<int16_t>(-1234)), "-1234");
+        CHECK_EQUAL(Ext::ToString(static_cast<int32_t>(-123456)), "-123456");
+        CHECK_EQUAL(Ext::ToString(static_cast<int64_t>(-123456)), "-123456");
+
+        CHECK_EQUAL(Ext::ToString(static_cast<uint8_t>(123)), "123");
+        CHECK_EQUAL(Ext::ToString(static_cast<uint16_t>(1234)), "1234");
+        CHECK_EQUAL(Ext::ToString(static_cast<uint32_t>(123456)), "123456");
+        CHECK_EQUAL(Ext::ToString(static_cast<uint64_t>(123456)), "123456");
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, Strings) {
+        CHECK_EQUAL(Ext::ToString("Hello world"), "Hello world");
+        CHECK_EQUAL(Ext::ToString(std::string("Hello world")), "Hello world");
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+
+    TEST_CASE(ToStringTest, CustomType) {
+        CustomType value { 1234 };
+        CHECK_EQUAL(Ext::ToString(value), "[CustomType] 1234");
+    }
+
+}
