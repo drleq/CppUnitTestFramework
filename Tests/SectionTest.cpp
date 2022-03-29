0f66e2ab9185a722f9f73ba69cab84e141731811
Andrew Condie <Andrew.Condie@mre.medical.canon>
2022-03-29 18:12:17 +0100

    Fixing make_tags_array() to be constexpr again.

--- Tests/SectionTest.cpp
+++ Tests/SectionTest.cpp
@@ -100,7 +100,7 @@ namespace CppUnitTestFrameworkTest {
             static constexpr std::string_view SourceFile = __FILE__;
             static constexpr size_t SourceLine = __LINE__;
             static constexpr std::string_view Name = "SectionTest::Nesting";
-            static std::vector<std::string_view> Tags;
+            static constexpr auto Tags = make_tags_array();
 
             void Run() override {
                 CHECK_EQUAL(GetTestLog(), "");
@@ -116,7 +116,6 @@ namespace CppUnitTestFrameworkTest {
                 CHECK_EQUAL(GetTestLog(), "Push Section: Outer\nPush Section: Inner\nPop\nPop\n");
             }
         };
-        std::vector<std::string_view> TestCase_Nesting::Tags = make_tags_array();
         TestRegistry::AutoReg<TestCase_Nesting> s_test_registrar_Nesting;
     }
 
@@ -129,7 +128,7 @@ namespace CppUnitTestFrameworkTest {
             static constexpr std::string_view SourceFile = __FILE__;
             static constexpr size_t SourceLine = __LINE__;
             static constexpr std::string_view Name = "SectionTest::BDD";
-            static std::vector<std::string_view> Tags;
+            static constexpr auto Tags = make_tags_array();
 
             void Run() override {
                 CHECK_EQUAL(GetTestLog(), "");
@@ -157,7 +156,6 @@ Pop
                 );
             }
         };
-        std::vector<std::string_view> TestCase_BDD::Tags = make_tags_array();
         TestRegistry::AutoReg<TestCase_BDD> s_test_registrar_BDD;
     }
 
